e7c058eda364c021b4a9ab14861359dd06b3ca85
Blizzardfur <111100393+Blizzardfur-Maxxx@users.noreply.github.com>
2024-01-12 14:28:03 -0600

    number instead of hex

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
 import socket
 
-
-#connection settings and connect packet
+# connection settings and connect packet
 print("Welcome to Shitty Classic Client pick a playername\n")
 name = input("Pick Playername\n")
 ip = input("Whats The Server IP Adress\n")
 port = input("Whats The Server Port\n")
-pvn = input("Whats The Server Protocol Hex example: x06 being pvn 6 \n")
-if pvn.startswith('x'):
-    pvn = pvn[1:]
+pvn_hex = input("Whats The Server Protocol Numbber \n")
+if pvn_hex.startswith('x'):
+    pvn_hex = pvn_hex[1:]
+pvn_int = int(pvn_hex, 16)
+pvn = bytes([pvn_int])
 packet = bytearray()
 packet += b"\x00"
-packet += bytes.fromhex(pvn)
+packet += pvn
 packet += name.ljust(64).encode("ascii")
 packet += "-".ljust(64).encode("ascii")
 packet += b"\x00"
 
-#TCP Socket Horrors
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-client.connect((ip, int(port))) 
+# TCP Socket Horrors
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((ip, int(port)))
 client.send(packet)
 
 while True:
-    #chat handler
+    # chat handler
     message = input("Type a Message\n")
     packet2 = bytearray()
     packet2 += b"\x0d"
     packet2 += bytes([0xFF])
     packet2 += message.ljust(64).encode("ascii")
 
-    #TCP Socket Horrors 2
+    # TCP Socket Horrors 2
     client.send(packet2)
-    print("Sent Message!!") 
+    print("Sent Message!!")
     if "/stop" in message:
-            client.close()
-            break
+        client.close()
+        break
